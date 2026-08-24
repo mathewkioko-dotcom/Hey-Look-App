1f01a9ca07b074401109338a123ba12d95d055b7
kiokomathew85-dev <kiokomathew85@gmail.com>
2026-08-24 21:00:35 +0300

    Update Hey Look App

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -241,7 +241,7 @@ export const MainLayout: React.FC<MainLayoutProps> = ({
                 HeyLook
               </span>
               <span className="block text-[10px] text-slate-400 font-semibold tracking-wider">
-                WHATSAPP • FB • IG
+                MATHEW PRESENTS
               </span>
             </div>
           </div>
