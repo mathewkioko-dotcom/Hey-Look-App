a5aacf1cdd0cb09fef46b459f8f16a04a0d8dda1
kiokomathew85-dev <kiokomathew85@gmail.com>
2026-08-24 17:24:38 +0300

    Update Hey Look App

--- src/App.tsx
+++ src/App.tsx
@@ -35,11 +35,11 @@ class AppErrorBoundary extends React.Component<
             Reload HeyLook
           </button>
           <p className="mt-12 text-center text-[10px] font-semibold tracking-[0.3em] text-slate-500">
-            MATHEW PRESENTS
+            ༒🅼︎🅰️︎🆃︎🅷︎🅴︎🅦︎ 🅟︎🅡︎🅔︎🅢︎🅔︎🅝︎🅣︎🅢︎ ⬤⬤⬤ ♛
           </p>
         </div>
       );
-    }
+    }``
     return this.props.children;
   }
 }
